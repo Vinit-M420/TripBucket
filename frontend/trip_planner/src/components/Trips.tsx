ae62520af3522f112db50d5f8b5becc3ef95bce0
Vinit <vinit_motghare@outlook.com>
2025-09-01 23:54:24 +0530

    Fixed no banner yet part

--- frontend/trip_planner/src/components/Trips.tsx
+++ frontend/trip_planner/src/components/Trips.tsx
@@ -11,8 +11,8 @@ const Trips = () => {
             <h1 className="text-green-800 text-2xl mt-10 mb-5">Your Trips</h1>
 
             <div className="grid grid-flow-col grid-cols-3 gap-5">
-                <div className="col-span-1 flex flex-col border border-green-800 rounded-xl gap-2 cursor-pointer ">
-                    <img src={iceland} className='w-[400px] h-auto rounded-t-xl' />
+                <div className="col-span-1 flex flex-col border-2 border-green-800 rounded-xl gap-2 cursor-pointer ">
+                    <img src={iceland} className='w-full h-[70%] rounded-t-lg' />
                     <div className='flex justify-between items-center mx-5'>
                         <h1 className='text-green-800 text-xl font-semibold'>Iceland</h1>
                         <Lock />
@@ -26,7 +26,7 @@ const Trips = () => {
                     </div>
                     <div className='flex mx-5 gap-5'>
                         <button className="bg-green-800 text-white px-10 py-1 rounded-2xl hover:bg-green-700 
-                        transition-all duration-200 cursor-pointer mt-2">
+                        transition-all duration-200 cursor-pointer my-2">
                             Edit
                         </button>
                     </div>
@@ -35,25 +35,24 @@ const Trips = () => {
                     </div>
                 </div>
 
-                <div className="col-span-1 flex flex-col border border-green-800 rounded-xl gap-2 cursor-pointer">
-                    <div className='bg-green-50 w-[400px] h-[250px] rounded-t-xl'>
-                        {/* <h2>No Banner Yet</h2> */}
+                <div className="col-span-1 flex flex-col border-2 border-green-800 rounded-xl gap-2 cursor-pointer">
+                    <div className='bg-green-200 w-full h-[70%] rounded-t-lg place-content-center'>
+                            <h2 className='text-center text-green-800'>No Banner Yet</h2>
                     </div>
-                    <div className='flex justify-between items-center mx-3'>
+                    <div className='flex justify-between items-center mx-5'>
                         <h1 className='text-green-800 text-xl font-semibold'>Vietnam</h1>
                         <Lock />
                     </div>
-                    <div className='flex gap-2 mx-3 items-center'>
+                    <div className='flex gap-2 mx-5 items-center'>
                         <h2 className='text-green-600 text-sm'>Bucket List</h2>
                         <CheckCircle />
                     </div>
-                    <div className='mx-3'>
+                    <div className='mx-5'>
                         <h2 className='text-green-600 text-sm'>Someday ...</h2>
                     </div>
-                    <div className='flex mx-3 gap-5'>
-                        
+                    <div className='flex mx-5 gap-5'>
                         <button className="bg-green-800 text-white px-10 py-1 rounded-2xl hover:bg-green-700 
-                        transition-all duration-200 cursor-pointer mt-2">
+                        transition-all duration-200 cursor-pointer my-2">
                             Edit
                         </button>
                     </div>
